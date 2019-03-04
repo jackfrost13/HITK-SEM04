5b466594e90c51663d4c2b1c0d9467619fe4adef
Rohit Agarwal <36669463+jackfrost13@users.noreply.github.com>
2019-03-04 15:41:55 +0530

    uploaded in lab

---
+++ MATH2212(LAB)/program/lagrangeInterpolation.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int n;
+	printf("Enter n :");
+	scanf("%d",&n);
+	double x[n];
+	double fx[n];
+	int i,j;double x0;
+	double ans=0.0;
+	for(i=0;i<n;i++)
+	{
+		printf("Enter %dth (x,f(x)) :",i+1);
+		scanf("%lf%lf",&x[i],&fx[i]);
+	}
+	printf("Enter the interpolating point x0 : ");
+	scanf("%lf",&x0);
+	for(i=0;i<n;i++)
+	{
+		double a=1;
+	
+		for(j=0;j<n;j++)
+		{
+			if(j==i)
+			continue;
+			a=a*((double)(x0-x[j])/(x[i]-x[j]));
+		}
+		ans=ans+ a*fx[i];
+	}
+	printf("Answer is %lf\n",ans);
+}
