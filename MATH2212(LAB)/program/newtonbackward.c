f5e32492cd4785808e42b7220172d43d135505a8
devanshhalwai <devansh.halwai@gmail.com>
2019-05-06 23:05:59 +0530

    backward interpolation

--- MATH2212(LAB)/program/newtonbackward.c
+++ MATH2212(LAB)/program/newtonbackward.c
@@ -1,34 +1,36 @@
 #include<stdio.h>
 #include<math.h>
+
 void main()
 {
     int n,i,j;
-    float x,a[10][10];
+    float x;
     printf("enter the no of values\n");
     scanf("%d",&n);
+	float a[n][n+1];
     printf("enter the point to be interpolated\n");
     scanf("%f",&x);
     printf("enter the datapoints in the form (x,f(x))\n");
     for(i=0;i<n;i++)
         scanf("%f %f",&a[i][0],&a[i][1]);
-    for(j=2;j<=n;j++)
-        for(i=0;i<n+1-j;i++)
-            a[i][j]=a[i+1][j-1]-a[i][j-1];
+    for(j=2;j<n+1;j++)
+        for(i=j-1;i<n;i++)
+            a[i][j]=a[i][j-1]-a[i-1][j-1];
     printf("\nthe matrix\n");
     for(i=0;i<n;i++)
     {
-        for(j=0;j<n+1-i;j++)
+        for(j=0;j<n+1;j++)
             printf("%0.3lf\t",a[i][j]);
         printf("\n");
     }
 
-	float p = (x - a[0][0] )/ (a[1][0]-a[0][0]);
-	float yp=a[0][1];
+	float p = (x - a[n-1][0])/(a[1][0]-a[0][0]);
+	float yp=a[n-1][1];
 	float k=1;
 	for(j=2;j<=n;j++)
 	{
-		k=k*(p+2-j)/(j-1);
-		yp+=k*a[0][j];
+		k=k*(p+j-2)/(j-1);
+		yp+=k*a[n-1][j];
 	}
-	printf("Final ans f(%f) is %f\n",p,yp);
+	printf("Final ans f(%f) is %f\n",x,yp);
 }
